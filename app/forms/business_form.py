8faa33c60e806d6a1973a87664123222caaeba5b
David <david_gu@yahoo.com>
2023-10-06 13:16:05 -0700

    changes to db

---
+++ app/forms/business_form.py
@@ -0,0 +1,18 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, URLField
+from wtforms.validators import DataRequired
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+from ..api.AWS_helpers import AUDIO_ALLOWED_EXTENSIONS, IMAGE_ALLOWED_EXTENSIONS
+
+"""
+need to add enctype="multipart/form-data" to any form tag
+on the template for this form for AWS to work
+"""
+
+class BusinessForm(FlaskForm):
+    name = StringField('name', validators=[DataRequired()])
+    # image = URLField('Image URL', validators=[DataRequired()])
+    image = FileField('Image URL', validators=[FileRequired(), FileAllowed(list(IMAGE_ALLOWED_EXTENSIONS))])
+    # audio = URLField('Audio URL', validators=[DataRequired()])
+    audio = FileField('Audio URL', validators=[FileRequired(), FileAllowed(list(AUDIO_ALLOWED_EXTENSIONS))])
+    submit = SubmitField('Create New Business')
